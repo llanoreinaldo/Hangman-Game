95d939f24d60afa37cedbe5ed7461efe46b72126
Reinaldo Llano, Jr <llanoreinaldo@gmail.com>
2018-03-07 18:34:59 -0500

    Commit changes to game.js and index to fix the bugs!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,6 @@ var puzzlesRuisms = ["sissy that walk", "kitty girl", "you better work", "bring 
     lettersTried = [],
     wrongLetters = [],
     guessesLeft = 9,
-    rightLetters = [],
     themeAudio = new Audio('assets/sounds/rpdr-theme.mp3'),
     introAudio = new Audio('assets/sounds/start-your-engines.mp3'),
     wrongAudio = new Audio('assets/sounds/calm-down-beyonce.mp3'),
@@ -16,7 +15,8 @@ var puzzlesRuisms = ["sissy that walk", "kitty girl", "you better work", "bring 
     lostAudio = new Audio('assets/sounds/sashay-away.mp3'),
     tryAudio = new Audio('assets/sounds/not-today-satan.mp3'),
     finalGuessAudio = new Audio('assets/sounds/the-time-has-come.mp3'),
-    underScore = [];
+    underScore = [],
+    randRu = '';
 
 //DOM Declarations for hooking into
 var DOMwordGuess = document.getElementById('blanks'),
@@ -26,11 +26,7 @@ var DOMwordGuess = document.getElementById('blanks'),
     DOMlossCounter = document.getElementById('losses'),
     DOMplayGame = document.getElementById('myPlay-button');
 
-//plays intro theme song
-themeAudio.play("intro");
 
-//adds event listener for user click to start game
-DOMplayGame.addEventListener('click', startGame);
 
 
 //Start Game Function
@@ -39,27 +35,23 @@ function startGame() {
     lettersTried = [];
     wrongLetters = [];
     guessesLeft = 9;
-    rightLetters = [];
     underScore = [];
-    document.getElementById("image").setAttribute("src", "assets/images/hangmanben9.JPG");
+    document.getElementById('image').src = "assets/images/hangmanben9.JPG";
     document.getElementById("numGuesses").textContent = "Start Guessing";
     introAudio.play();
 
     //Randomize word choice
-    var randRu = puzzlesRuisms[Math.floor(Math.random() * puzzlesRuisms.length)];
-
+    randRu = puzzlesRuisms[Math.floor(Math.random() * puzzlesRuisms.length)];
 
     // Randomly loops through the random word choice
-    function underScore() {
-        for (var i = 0; i < randRu.length; i++) {
+    for (var i = 0; i < randRu.length; i++) {
 
-            //pushes blank spaces for random word if there is a space in the word
-            if (randRu[i] === " ") {
-                underScore.push('');
-            } else {
-                underScore.push("_")
+        //pushes blank spaces for random word if there is a space in the word
+        if (randRu[i] === " ") {
+            underScore.push('');
+        } else {
+            underScore.push("_")
 
-            }
         }
     }
     //Displays relevant DOM's to HTML
@@ -67,92 +59,113 @@ function startGame() {
     DOMwrongGuesses.textContent = wrongLetters
     DOMguessesLeft.textContent = guessesLeft;
 
+    //records user key
+    document.onkeyup = function (event) {
+        //checks key pressed is a valid key
+        console.dir(event.key);
+
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            //records valid letter pressed
+            userGuess(event.key)
+        }
+    }
 }
 
 //sets the base image for hangman game
-function hangmanImage() {
-    var imgNumber = guessesLeft
-    document.getElementById("image").setAttribute("src", "assets/images/hangmanben" + imgNumber + ".jpg");
+function setImage() {
+    if (guessesLeft === 8) {
+        document.getElementById('image').src = "assets/images/hangmanben8.JPG";
+    }
+    if (guessesLeft === 7) {
+        document.getElementById('image').src = "assets/images/hangmanben7.JPG";
+    }
+    if (guessesLeft === 6) {
+        document.getElementById('image').src = "assets/images/hangmanben6.JPG";
+    }
+    if (guessesLeft === 5) {
+        document.getElementById('image').src = "assets/images/hangmanben5.JPG";
+    }
+    if (guessesLeft === 4) {
+        document.getElementById('image').src = "assets/images/hangmanben4.JPG";
+    }
+    if (guessesLeft === 3) {
+        document.getElementById('image').src = "assets/images/hangmanben3.JPG";
+    }
+    if (guessesLeft === 2) {
+        document.getElementById('image').src = "assets/images/hangmanben2.JPG";
+    }
+    if (guessesLeft === 1) {
+        document.getElementById('image').src = "assets/images/hangmanben1.JPG";
+    }
+    if (guessesLeft === 0) {
+        document.getElementById('image').src = "assets/images/hangmanbenloser1.JPG";
+    }
 }
+
+//adds event listener for user click to start game
+DOMplayGame.addEventListener('click', startGame);
 
 
 //Records user guesses and matches against the random word chosen from the array
 
-//records user key
-document.onkeydown = function (event) {
 
-    //checks key pressed is a valid key
-    console.dir(event);
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-        //records valid letter pressed
-        userGuess(event.key)
-    }
-}
 
 //checks the letter inside the random word 
 function userGuess(letter) {
 
-    if (playGame === true && lettersTried.indexOf(letter) === -1) {
+    if (playGame && lettersTried.indexOf(letter) === -1) {
         lettersTried.push(letter);
         //checks letter inside random word choice array
         for (var i = 0; i < randRu.length; i++) {
-            if (randRu[i].tolowerCase() === letter.tolowerCase()) {
-                underScore[i] = randRu[i];
+            if (randRu[i].toLowerCase() === letter.toLowerCase()) {
+                underScore[i] = letter;
                 rightAudio.play()
             }
         }
 
-        DOMwordGuess.textContent = underScore.join(" ");
-        checkWrongGuess(letter);
+        DOMwordGuess.textContent = underScore.join(' ');
+        checkWrong(letter);
 
-    } else {
-        //alerts user how to start a new game
-        if (!playGame) {
-            alert("Hello Henny! Press any key to play again")
-        } else {
-            alert("Sorry henny, you already tried that letter. Pick another!")
-        }
+    } else if (lettersTried.indexOf(letter) > -1) {
+        alert("Sorry henny, you already tried that letter. Pick another!")
     }
-
 }
 
 // Checks wrong guess letters and adds wrong letters to array
-function checkWrongGuess(letter) {
-    if (underScore.indexOf(letter.tolowerCase()) === -1 && underScore.indexOf(letter.toUpperCase()) === -1) {
+function checkWrong(letter) {
+    if (underScore.indexOf(letter.toLowerCase()) === -1) {
         guessesLeft--;
         wrongLetters.push(letter);
-        DOMwrongGuesses.textContent = wrongLetters.join(" ");
+        DOMwrongGuesses.textContent = wrongLetters.join(' ');
         DOMguessesLeft.textContent = guessesLeft;
         wrongAudio.play()
     }
-    Losses()
-    hangmanImage()
+    setImage()
+    checkLosses()
 }
 
 //Game logic for win/losses
 function Win() {
 
     //counter is cutting off one short of the word... need to FIX!
-    if (randRu.tolowerCase() === underScore.join('').tolowerCase()) {
+    if (randRu.toLowerCase() === underScore.join('').toLowerCase()) {
         playGame = false;
         wins++;
         wonAudio.play();
         DOMwinCounter = wins;
-        DOMwordGuess.textContent = randRu
         document.getElementById("image").setAttribute("src", "assets/images/Winner.jpg")
     }
 }
 
-function Losses() {
+function checkLosses() {
 
-    if (guessesLeft === 0); {
+    if (guessesLeft === 0) {
         lostAudio.play()
         losses++;
         playGame = false;
-        DOMwordGuess.textContent = randRu
         DOMlossCounter = losses;
-        document.getElementById("image").setAttribute("src", "assets/images/loser1.jpg")
 
     }
     Win()
+
 }
